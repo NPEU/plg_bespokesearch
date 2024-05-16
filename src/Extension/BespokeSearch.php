96684cef38a3ad2722fb0f656aa00c164b6cc79b
Andy Kirk <andy@andykirk.net>
2024-05-16 17:45:33 +0100

    J4

--- src/Extension/BespokeSearch.php
+++ src/Extension/BespokeSearch.php
@@ -3,73 +3,111 @@
  * @package     Joomla.Plugin
  * @subpackage  Finder.BespokeSearch
  *
- * @copyright   Copyright (C) NPEU 2019.
+ * @copyright   Copyright (C) NPEU 2024.
  * @license     MIT License; see LICENSE.md
  */
 
+namespace NPEU\Plugin\Finder\BespokeSearch\Extension;
+
 defined('_JEXEC') or die;
 
-require_once JPATH_ADMINISTRATOR . '/components/com_finder/helpers/indexer/adapter.php';
+use Joomla\CMS\Categories\Categories;
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Table\Table;
+use Joomla\Component\Finder\Administrator\Indexer\Adapter;
+use Joomla\Component\Finder\Administrator\Indexer\Helper;
+use Joomla\Component\Finder\Administrator\Indexer\Indexer;
+use Joomla\Component\Finder\Administrator\Indexer\Result;
+use Joomla\Component\Weblinks\Site\Helper\RouteHelper;
+use Joomla\Database\DatabaseAwareTrait;
+use Joomla\Database\DatabaseInterface;
+use Joomla\Database\DatabaseQuery;
+use Joomla\Event\DispatcherInterface;
+use Joomla\Registry\Registry;
+use Joomla\Utilities\ArrayHelper;
+
 
 /**
  * Allows indexing of certain Bespoke modules.
  */
-class plgFinderBespokeSearch extends FinderIndexerAdapter
+final class BespokeSearch extends Adapter
 {
-    protected $autoloadLanguage = true;
+    use DatabaseAwareTrait;
+
+    /**
+     * An internal flag whether plugin should listen any event.
+     *
+     * @var bool
+     *
+     * @since   4.3.0
+     */
+    protected static $enabled = false;
+
+    /**
+     * The plugin identifier.
+     *
+     * @var    string
+     * @since  2.5
+     */
+    protected $context = 'BespokeSearch';
 
     /**
      * The extension name.
      *
      * @var    string
+     * @since  2.5
      */
     protected $extension = 'com_bespoke';
+
+    /**
+     * The sublayout to use when rendering the results.
+     *
+     * @var    string
+     * @since  2.5
+     */
+    #protected $layout = 'weblink';
 
     /**
      * The type of content that the adapter indexes.
      *
      * @var    string
+     * @since  2.5
      */
-    protected $type_title = 'Bespokesearch';
+    protected $type_title = 'BespokeSearch';
 
     /**
-     * Method to setup the adapter before indexing.
+     * The table name.
      *
-     * @return  boolean  True on success, false on failure.
-     *
-     * @throws  Exception on database error.
+     * @var    string
+     * @since  2.5
      */
-    protected function setup()
+    #protected $table = '#__bespoke';
+
+    /**
+     * Load the language file on instantiation.
+     *
+     * @var    boolean
+     * @since  3.1
+     */
+    protected $autoloadLanguage = true;
+
+    /**
+     * Constructor
+     *
+     * @param   DispatcherInterface  $dispatcher
+     * @param   array                $config
+     * @param   DatabaseInterface    $database
+     */
+    public function __construct(DispatcherInterface $dispatcher, array $config, DatabaseInterface $database)
     {
-        // CLI indexer throws lots of notices about JPATH_COMPONENT so defining here.
-        // Note this is probably only CLI-specific, and is in place to make it easier to see
-        // problems in THIS plugin when testing via the CLI, so keep commented unless testing.
-        /*if (!defined('JPATH_COMPONENT')) {
-            define('JPATH_COMPONENT', '');
-        }*/
-        return true;
+        self::$enabled = true;
+
+        parent::__construct($dispatcher, $config);
+
+        $this->setDatabase($database);
     }
 
-    /**
-     * Method to index an item.
-     *
-     * @param   FinderIndexerResult  $item  The item to index as a FinderIndexerResult object.
-     *
-     * @return  boolean  True on success.
-     *
-     * @throws  Exception on database error.
-     */
-    protected function index(FinderIndexerResult $item)
-    {
-        // Check if the extension is enabled
-        if (JComponentHelper::isEnabled($this->extension) == false) {
-            return;
-        }
-
-        $this->indexer->index($item);
-    }
-
-    /**
+        /**
      * Method to get the MenuItems.
      *
      *
@@ -77,7 +115,7 @@ class plgFinderBespokeSearch extends FinderIndexerAdapter
      */
     protected function getMenuItems()
     {
-        $db = JFactory::getDbo();
+        $db = $this->getDatabase();
 
         // Get the COM_DESPOKE component id:
         $query = $db->getQuery(true);
@@ -150,11 +188,9 @@ class plgFinderBespokeSearch extends FinderIndexerAdapter
                 continue;
             }
 
-
             $menu_item['summary']    = $summary;
             $menu_item['start_date'] = $start_date;
         }
-
         return $menu_items;
     }
 
@@ -163,27 +199,26 @@ class plgFinderBespokeSearch extends FinderIndexerAdapter
      *
      * @param   integer         $offset  The list offset.
      * @param   integer         $limit   The list limit.
-     * @param   JDatabaseQuery  $query   A JDatabaseQuery object. [optional]
+     * @param   QueryInterface  $query   A QueryInterface object. [optional]
      *
-     * @return  array  An array of FinderIndexerResult objects.
+     * @return  Result[]  An array of Result objects.
      *
-     * @throws  Exception on database error.
+     * @since   2.5
+     * @throws  \Exception on database error.
      */
     protected function getItems($offset, $limit, $query = null)
     {
-        $items = array();
+        $items = [];
 
         // Get the content items to index.
         //$this->db->setQuery($this->getListQuery($query), $offset, $limit);
         //$rows = $this->db->loadAssocList();
         $rows = $this->getMenuItems();
 
-        #var_dump($rows); exit;
-
         // Convert the items to result objects.
         foreach ($rows as $row) {
             // Convert the item to a result object.
-            $item = JArrayHelper::toObject($row, 'FinderIndexerResult');
+            $item = ArrayHelper::toObject($row, Result::class);
 
             // Sort out endcoding stuff:
             #$item->summary  = $this->utf8_convert($item->summary);
@@ -210,35 +245,39 @@ class plgFinderBespokeSearch extends FinderIndexerAdapter
             // Add the item to the stack.
             $items[] = $item;
         }
-
         return $items;
     }
 
     /**
-     * Method to convert utf8 characters.
-     * (not currently used but keep in case)
+     * Method to index an item. The item must be a Result object.
      *
-     * @param   string   $text  The text to convert.
+     * @param   Result  $item  The item to index as an Result object.
      *
-     * @return  string
+     * @return  void
+     *
+     * @throws  \Exception on database error.
+     * @since   2.5
+     */
+    protected function index(Result $item)
+    {
+        // Check if the extension is enabled
+        if (ComponentHelper::isEnabled($this->extension) == false) {
+            return;
+        }
+
+        $item->setLanguage();
+        $this->indexer->index($item);
+    }
+
+    /**
+     * Method to setup the indexer to be run.
+     *
+     * @return  boolean  True on success.
      *
      * @since   2.5
      */
-    /*protected function utf8_convert($text)
+    protected function setup()
     {
-        if (!is_string($text)) {
-            trigger_error('Function \'utf8_convert\' expects argument 1 to be a string', E_USER_ERROR);
-            return false;
-        }
-        // Only do the slow convert if there are 8-bit characters
-        // Avoid using 0xA0 (\240) in ereg ranges. RH73 does not like that
-        if (!preg_match("[\200-\237]", $text) && !preg_match("[\241-\377]", $text)) {
-            return $text;
-        }
-        // Decode three byte unicode characters
-        $text = preg_replace("/([\340-\357])([\200-\277])([\200-\277])/e", "'&#'.((ord('\\1')-224)*4096 + (ord('\\2')-128)*64 + (ord('\\3')-128)).';'", $text);
-        // Decode two byte unicode characters
-        $text = preg_replace("/([\300-\337])([\200-\277])/e", "'&#'.((ord('\\1')-192)*64+(ord('\\2')-128)).';'", $text);
-        return $text;
-    }*/
+        return true;
+    }
 }
